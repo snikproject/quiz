44e9cababa059701b8783864765739adc79cab77
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2019-06-26 12:34:32 +0200

    Set question count to 15.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import QuizApp from './components/QuizApp';
 import './style.css';
 
 render(
-  <QuizApp totalQuestions={3} />,
+  <QuizApp totalQuestions={15} />,
   document.getElementById('app')
 );
