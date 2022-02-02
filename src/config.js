88fac779ec8f0bd69610016deaf18054fcd5f923
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-02-02 14:46:05 +0100

    Increase default question count to 20.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const queryParams = new URLSearchParams(window.location.search);
 const config = {
   // 0 is unlimited
-  MAX_QUESTION_COUNT: parseInt(queryParams.get('count') ?? '10', 10),
+  MAX_QUESTION_COUNT: parseInt(queryParams.get('count') ?? '20', 10),
   // 0 is unlimited
   QUESTION_SECONDS: parseInt(queryParams.get('time') ?? '60', 10),
   DELAY_MS: parseInt(queryParams.get('delay') ?? '1200', 10),
