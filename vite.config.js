84d3343f9c923fae48510983ef35b3be5ba50f6b
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-02-07 10:25:24 +0100

    Support legacy browsers.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ]
+});
