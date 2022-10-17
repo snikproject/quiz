b46527a16b76bc3c5e41e4360abee3f3cbad1c90
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-10-17 09:01:55 +0200

    Set relative base path. Allows deploying to subdirectories.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
+  base: '', // relative paths
   plugins: [
     react(),
     legacy({
